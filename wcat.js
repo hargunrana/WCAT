f08dd43e30b7758603ad4ff54fd50f1691eb7210
hargunrana <hargun.rana1@gmail.com>
2022-03-20 14:49:57 +0530

    Implemented -n and -b

--- wcat.js
+++ wcat.js
@@ -37,15 +37,15 @@ for (let i = 0; i < filesArr.length; i++) {
     content += fileContent + "\n";
 }
 
-console.log(content);
-
 //----------------->Appling the Options<---------------------
 
 let contentArr = content.split("\n");
-console.table(contentArr);
+
+//----------------->Applying -s option<---------------------
 
 let isSPresent = optionsArr.includes("-s");
 
+let tempArr = [];
 if (isSPresent) {
     for (let i = 1; i < contentArr.length; i++) {
         if (
@@ -55,14 +55,63 @@ if (isSPresent) {
             contentArr[i] = null;
         }
     }
-    let tempArr = [];
+
     for (let i = 0; i < contentArr.length; i++) {
         if (contentArr[i] != null) {
             tempArr.push(contentArr[i]);
         }
     }
-    console.log("data after removing extra lines\n", tempArr);
+}
+contentArr = tempArr;
+
+let isNPresent = optionsArr.includes("-n");
+let isBPresent = optionsArr.includes("-b");
+
+//----------------->Appling -n and -b options<---------------------
+
+// Getting index of -n and -b
+let indexOfN = optionsArr.indexOf("-n");
+let indexOfB = optionsArr.indexOf("-b");
+
+let finalOption = "";
+
+// if both -n and -b are present in the optionsArr
+if (indexOfN != -1 && indexOfB != -1) {
+    if (indexOfN < indexOfB) {
+        finalOption = "-n";
+    } else {
+        finalOption = "-b";
+    }
+} else {
+    if (indexOfN != -1) {
+        finalOption = "-n";
+    } else if (indexOfB != -1) {
+        finalOption = "-b";
+    }
+}
+// Running the Final Option
+if (finalOption == "-n") {
+    modifyContentByN();
+} else {
+    modifyContentByB();
 }
 
+// Function to run -n
+function modifyContentByN() {
+    for (let i = 0; i < contentArr.length; i++) {
+        contentArr[i] = i + 1 + ". " + contentArr[i];
+    }
+}
 
+// Function to run -b
+function modifyContentByB() {
+    let count = 1;
+    for (let i = 0; i < contentArr.length; i++) {
+        if (contentArr[i] != "") {
+            contentArr[i] = count + ". " + contentArr[i];
+            count++;
+        }
+    }
+}
 
+console.log(contentArr);
